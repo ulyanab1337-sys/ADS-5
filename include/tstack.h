c9f3846f400d087d08fcd03e53e171141e39c0ba
ulyanab1337-sys <ulyanab1337@gmail.com>
2026-04-13 10:25:38 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -3,8 +3,41 @@
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
-class TStack {
-  // добавьте код стека
+class Tstack {
+private:
+    T arr[size];
+    int top_idx;
+
+public:
+    Tstack() : top_idx(-1) {}
+
+    void push(T value) {
+        if (top_idx < size - 1) {
+            arr[++top_idx] = value;
+        }
+    }
+
+    T pop() {
+        if (top_idx >= 0) {
+            return arr[top_idx--];
+        }
+        return T();
+    }
+
+    T get() const {
+        if (top_idx >= 0) {
+            return arr[top_idx];
+        }
+        return T();
+    }
+
+    bool isEmpty() const {
+        return top_idx == -1;
+    }
+
+    void clear() {
+        top_idx = -1;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif // INCLUDE_TSTACK_H_
